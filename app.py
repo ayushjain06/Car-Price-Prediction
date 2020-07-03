36da41812ccfb492af6f1904448972a4aadba18c
ayushjain06 <67739742+ayushjain06@users.noreply.github.com>
2020-07-03 20:19:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ def predict():
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         elif output < 0.6*Present_Price:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('index.html',prediction_text="You Can Sell the Car at {}".format(output))
         else :
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output2))
+            return render_template('index.html',prediction_text="You Can Sell the Car at {}".format(output2))
     else:
         return render_template('index.html')
 
